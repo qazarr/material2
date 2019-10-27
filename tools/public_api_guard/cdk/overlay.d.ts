56fdba564058a7be57d5569a346f49cfb3bd29f7
crisbeto <crisbeto@abv.bg>
2019-10-27 11:37:04 +0100

    refactor(overlay): remove deprecated connected position strategy
    The `ConnectedPositionStrategy` has been deprecated since at least 7.0.0 and is just a wrapper around `FlexibleConnectedPositionStrategy`. These changes remove it since it still adds a fair bit of code that has to be maintained.
    
    BREAKING CHANGES:
    * `ConnectedPositionStrategy` has been removed. Use `FlexibleConnectedPositionStrategy` instead.
    * `OverlayPositionBuilder.connectedTo` has been removed. Use `OverlayPositionBuilder.flexibleConnectedTo` instead.

--- tools/public_api_guard/cdk/overlay.d.ts
+++ tools/public_api_guard/cdk/overlay.d.ts
@@ -87,28 +87,6 @@ export interface ConnectedPosition {
     weight?: number;
 }
 
-export declare class ConnectedPositionStrategy implements PositionStrategy {
-    get _isRtl(): boolean;
-    _positionStrategy: FlexibleConnectedPositionStrategy;
-    _preferredPositions: ConnectionPositionPair[];
-    get onPositionChange(): Observable<ConnectedOverlayPositionChange>;
-    get positions(): ConnectionPositionPair[];
-    constructor(originPos: OriginConnectionPosition, overlayPos: OverlayConnectionPosition, connectedTo: ElementRef<HTMLElement>, viewportRuler: ViewportRuler, document: Document, platform: Platform, overlayContainer: OverlayContainer);
-    apply(): void;
-    attach(overlayRef: OverlayReference): void;
-    detach(): void;
-    dispose(): void;
-    recalculateLastPosition(): void;
-    setOrigin(origin: ElementRef): this;
-    withDirection(dir: 'ltr' | 'rtl'): this;
-    withFallbackPosition(originPos: OriginConnectionPosition, overlayPos: OverlayConnectionPosition, offsetX?: number, offsetY?: number): this;
-    withLockedPosition(isLocked: boolean): this;
-    withOffsetX(offset: number): this;
-    withOffsetY(offset: number): this;
-    withPositions(positions: ConnectionPositionPair[]): this;
-    withScrollableContainers(scrollables: CdkScrollable[]): void;
-}
-
 export declare class ConnectionPositionPair {
     offsetX?: number | undefined;
     offsetY?: number | undefined;
@@ -252,7 +230,6 @@ export declare class OverlayModule {
 
 export declare class OverlayPositionBuilder {
     constructor(_viewportRuler: ViewportRuler, _document: any, _platform: Platform, _overlayContainer: OverlayContainer);
-    connectedTo(elementRef: ElementRef, originPos: OriginConnectionPosition, overlayPos: OverlayConnectionPosition): ConnectedPositionStrategy;
     flexibleConnectedTo(origin: FlexibleConnectedPositionStrategyOrigin): FlexibleConnectedPositionStrategy;
     global(): GlobalPositionStrategy;
     static ɵfac: i0.ɵɵFactoryDef<OverlayPositionBuilder>;
