1ecc0348f28ca514d0e03125a5a73137651219d7
crisbeto <crisbeto@abv.bg>
2020-01-31 19:10:27 +0100

    fix(a11y): don't allow passing in null to FocusMonitor.focusVia
    Passing in null to the `FocusMonitor.focusVia` method is currently supported, but it doesn't make sense. These changes make it impossible via typings.

--- src/material/sidenav/drawer.ts
+++ src/material/sidenav/drawer.ts
@@ -352,7 +352,8 @@ export class MatDrawer implements AfterContentInit, AfterContentChecked, OnDestr
     if (activeEl && this._elementRef.nativeElement.contains(activeEl)) {
       // Note that we don't check via `instanceof HTMLElement` so that we can cover SVGs as well.
       if (this._elementFocusedBeforeDrawerWasOpened) {
-        this._focusMonitor.focusVia(this._elementFocusedBeforeDrawerWasOpened, this._openedVia);
+        this._focusMonitor.focusVia(this._elementFocusedBeforeDrawerWasOpened,
+            this._openedVia || 'program');
       } else {
         this._elementRef.nativeElement.blur();
       }
